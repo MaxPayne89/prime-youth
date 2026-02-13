4fd3d5db388ac83a524a289f1ededcdee3399549
MaxPayne89 <maxpergl@gmail.com>
2026-02-13 09:26:56 +0100

    refactor(boundary): disable output checks for Repo and Mailer exports
    Updated the Boundary usage in KlassHero to include a check option that disables output checks for the exported Repo and Mailer modules.

--- lib/klass_hero.ex
+++ lib/klass_hero.ex
@@ -7,5 +7,5 @@ defmodule KlassHero do
   if it comes from the database, an external API or others.
   """
 
-  use Boundary, deps: [], exports: [Repo, Mailer]
+  use Boundary, deps: [], exports: [Repo, Mailer], check: [out: false]
 end
