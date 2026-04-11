d04c58d8754ed7f5064911f1a9d88c60a8b7a068
Maximilian Pergl-Rasokat <maximilianpergl@maximiliansmini.speedport.ip>
2026-04-11 08:48:38 +0200

    feat: add count_by_provider_and_origin to program repository (#360)

--- lib/klass_hero/program_catalog/domain/ports/for_creating_programs.ex
+++ lib/klass_hero/program_catalog/domain/ports/for_creating_programs.ex
@@ -10,4 +10,9 @@ defmodule KlassHero.ProgramCatalog.Domain.Ports.ForCreatingPrograms do
 
   @callback create(program :: Program.t()) ::
               {:ok, Program.t()} | {:error, term()}
+
+  @callback count_by_provider_and_origin(
+              provider_id :: String.t(),
+              origin :: :self_posted | :business_assigned
+            ) :: non_neg_integer()
 end
